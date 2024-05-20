114017697541f08ece0882854ec2f53772f23a2d
Srijan Manandhar <srijan.manandhar@gmail.com>
2024-05-20 12:42:29 +0545

    Update media query and card width

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,34 +1,38 @@
 const viewPortWidth = window.innerWidth;
 const cardShareBtn = document.querySelector(".card__share-btn");
 const cardShareLinks = document.querySelector(".card__share-links");
+let popoverActive = false;
 
 function showShareLinks() {
-  if (viewPortWidth < 768) {
-    cardShareBtn.classList.add("slider-active");
+  popoverActive = true;
+  if (viewPortWidth < 1024) {
     cardShareLinks.classList.add("slider-active");
+    cardShareBtn.classList.add("slider-active");
   } else {
-    cardShareLinks.classList.add("popover-active");
+    cardShareLinks.classList.add("popover-show");
+    cardShareBtn.classList.add("popover-show");
   }
 }
 
 function hideShareLinks() {
-  if (viewPortWidth < 768) {
-    cardShareBtn.classList.remove("slider-active");
+  popoverActive = false;
+  if (viewPortWidth < 1024) {
     cardShareLinks.classList.remove("slider-active");
+    cardShareBtn.classList.remove("slider-active");
   } else {
-    cardShareLinks.classList.remove("popover-active");
+    cardShareLinks.classList.remove("popover-show");
+    cardShareBtn.classList.remove("popover-show");
   }
 }
 
-const showShareLinksEvents = ["mouseover", "touchstart"];
-showShareLinksEvents.forEach((event) => {
+const toggleShareLinksEvents = ["click", "touchstart", "keydown"];
+toggleShareLinksEvents.forEach((event) => {
   cardShareBtn.addEventListener(event, (e) => {
-    showShareLinks();
-  });
-});
-const hideShareLinksEvents = ["mouseout", "touchend"];
-hideShareLinksEvents.forEach((event) => {
-  cardShareBtn.addEventListener(event, (e) => {
-    hideShareLinks();
+    if (e.type === "keydown" && (e.key !== "Enter" || e.key !== " ")) {
+      return;
+    } else {
+      e.preventDefault();
+    }
+    !popoverActive ? showShareLinks() : hideShareLinks();
   });
 });
