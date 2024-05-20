9014ddceca9fe0486228835f65ab61aad7228cc9
Srijan Manandhar <srijan.manandhar@gmail.com>
2024-05-20 11:05:13 +0545

    added share button active interaction on mobile view

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,34 @@
-console.log("Hello from app.js!");
+const viewPortWidth = window.innerWidth;
+const cardShareBtn = document.querySelector(".card__share-btn");
+const cardShareLinks = document.querySelector(".card__share-links");
+
+function showShareLinks() {
+  if (viewPortWidth < 768) {
+    cardShareBtn.classList.add("slider-active");
+    cardShareLinks.classList.add("slider-active");
+  } else {
+    cardShareLinks.classList.add("popover-active");
+  }
+}
+
+function hideShareLinks() {
+  if (viewPortWidth < 768) {
+    cardShareBtn.classList.remove("slider-active");
+    cardShareLinks.classList.remove("slider-active");
+  } else {
+    cardShareLinks.classList.remove("popover-active");
+  }
+}
+
+const showShareLinksEvents = ["mouseover", "touchstart"];
+showShareLinksEvents.forEach((event) => {
+  cardShareBtn.addEventListener(event, (e) => {
+    showShareLinks();
+  });
+});
+const hideShareLinksEvents = ["mouseout", "touchend"];
+hideShareLinksEvents.forEach((event) => {
+  cardShareBtn.addEventListener(event, (e) => {
+    hideShareLinks();
+  });
+});
